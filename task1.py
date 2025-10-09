dcb35124350bf39a3e69ed7a8bf2150f033a7722
Steven-19-2 <stevenmartindsouza123@gmail.com>
2025-10-09 14:31:46 +0530

    Commit 1

--- task1.py
+++ task1.py
@@ -10,13 +10,15 @@ class Employee:
     empid: int
     name: str
     salary: float
-    salary_date: str
+    salary_date: date
 
 class GenerateData :   
-    employees = [] 
-    def generate_employee_data(self) :
+    def __init__(self):
+        self.employees = []
         self.fake = Faker()
-        
+
+
+    def generate_employee_data(self) :
         for i in range(10):
             empid= i+1
             name= self.fake.name()
@@ -25,7 +27,7 @@ class GenerateData :
             self.employees.append(Employee(empid, name, salary, salary_date))
     
     
-    def save_data_to_file(self, filename='employees_data1.csv'):
+    def save_data_to_file(self, filename):
         with open(filename, mode="w", newline='') as file:
             writer = csv.writer(file)
             writer.writerow(["empid", "name", "salary", "salary_date"])
@@ -38,6 +40,6 @@ if __name__ == "__main__":
     generator.generate_employee_data()
     # for emp in generator.employees:
     #     print(emp)
-    generator.save_data_to_file()
+    generator.save_data_to_file(filename='employees_data1.csv')
             
             
