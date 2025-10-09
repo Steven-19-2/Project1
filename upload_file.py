ba27f6c4c2ac423808117d13c465d4f03bfce94c
Steven-19-2 <stevenmartindsouza123@gmail.com>
2025-10-09 14:15:58 +0530

    Commit 1

--- upload_file.py
+++ upload_file.py
@@ -1,10 +1,16 @@
 import boto3
 #folder_path = "s3://zmx-training-bucketbatch2025/Steven/"
-
 s3 = boto3.client('s3')
-BUCKET_NAME = 'zmx-training-bucketbatch2025'
-FILE_NAME = 'employee_data.parquet'
+class FileUpload:
+    def __init__(self):
+        pass
+    
+    def upload_file(self, file_name, bucket):
+        s3.upload_file(file_name, bucket, 'Steven/'+file_name)
+        print('File is uploaded')
 
-s3.upload_file(FILE_NAME, BUCKET_NAME, 'Steven/'+FILE_NAME)
-
-print('file is uploaded')
+if __name__ == "__main__":
+    uploader = FileUpload()
+    BUCKET_NAME = 'zmx-training-bucketbatch2025'
+    FILE = 'employee_data.parquet'
+    uploader.upload_file(FILE, BUCKET_NAME)
